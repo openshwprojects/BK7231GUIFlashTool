63bbcaace4a35d64a1d37d9e5551e4e598b55b73
openshwprojects <openshwprojects@gmail.com>
2023-05-06 09:36:51 +0200

    obk config tool, unfinsihed, not called anywhere

---
+++ BK7231Flasher/ConfigBase.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BK7231Flasher
+{
+    class ConfigBase
+    {
+        protected byte[] raw = new byte[3584];
+
+        protected void writeByte(int ofs, byte b)
+        {
+            raw[ofs] = b;
+        }
+        protected byte readByte(int ofs)
+        {
+            return raw[ofs];
+        }
+        protected void writeStr(int ofs, string value, int maxLen)
+        {
+            byte[] strBytes = Encoding.ASCII.GetBytes(value);
+            int len = strBytes.Length;
+            if (len > maxLen)
+                len = maxLen;
+            for(int i = 0; i < len; i++)
+            {
+                writeByte(ofs + i, strBytes[i]);
+            }
+        }
+        protected string readStr(int ofs, int maxLen)
+        {
+            string r = "";
+            r = Encoding.ASCII.GetString(raw, ofs, maxLen);
+            return r;
+        }
+        protected void writeInt(int ofs, int value)
+        {
+            raw[ofs] = (byte)(value >> 24);
+            raw[ofs + 1] = (byte)(value >> 16);
+            raw[ofs + 2] = (byte)(value >> 8);
+            raw[ofs + 3] = (byte)value;
+        }
+        protected int readInt(int ofs)
+        {
+            int value = 0;
+            value |= raw[ofs] << 24;
+            value |= raw[ofs + 1] << 16;
+            value |= raw[ofs + 2] << 8;
+            value |= raw[ofs + 3];
+            return value;
+        }
+    }
+}
---
+++ BK7231Flasher/OBKConfig.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BK7231Flasher
+{
+    class OBKConfig : ConfigBase
+    {
+        static int OBK_CONFIG_VERSION = 4;
+        static int MAX_GPIO = 32;
+        static int MAX_CHANNELS = 64;
+
+        public int version
+        {
+            get
+            {
+                return readInt(0x04);
+            }
+            set
+            {
+                writeInt(0x04,value);
+            }
+        }
+        public int genericFlags
+        {
+            get
+            {
+                return readInt(0x08);
+            }
+            set
+            {
+                writeInt(0x08, value);
+            }
+        }
+        public byte timeRequiredToMarkBootSuccessfull
+        {
+            get
+            {
+                return readByte(0x00000597);
+            }
+            set
+            {
+                writeByte(0x00000597, value);
+            }
+        }
+
+        public int genericFlags2
+        {
+            get
+            {
+                return readInt(0x0C);
+            }
+            set
+            {
+                writeInt(0x0C, value);
+            }
+        }
+        public string wifi_ssid
+        {
+            get
+            {
+                return readStr(0x14, 64);
+            }
+            set
+            {
+                writeStr(0x14, value, 64);
+            }
+        }
+        public string wifi_pass
+        {
+            get
+            {
+                return readStr(0x54, 64);
+            }
+            set
+            {
+                writeStr(0x54, value, 64);
+            }
+        }
+        public string mqtt_host
+        {
+            get
+            {
+                return readStr(0x94, 256);
+            }
+            set
+            {
+                writeStr(0x94, value, 256);
+            }
+        }
+        public string mqtt_clientId
+        {
+            get
+            {
+                return readStr(0x194, 64);
+            }
+            set
+            {
+                writeStr(0x194, value, 64);
+            }
+        }
+        public string mqtt_userName
+        {
+            get
+            {
+                return readStr(0x1D4, 64);
+            }
+            set
+            {
+                writeStr(0x1D4, value, 64);
+            }
+        }
+        public string mqtt_pass
+        {
+            get
+            {
+                return readStr(0x214, 128);
+            }
+            set
+            {
+                writeStr(0x214, value, 128);
+            }
+        }
+        public int mqtt_port
+        {
+            get
+            {
+                return readInt(0x294);
+            }
+            set
+            {
+                writeInt(0x294, value);
+            }
+        }
+        public string webappRoot
+        {
+            get
+            {
+                return readStr(0x298, 64);
+            }
+            set
+            {
+                writeStr(0x298, value, 64);
+            }
+        }
+        public string shortDeviceName
+        {
+            get
+            {
+                return readStr(0x2DE, 32);
+            }
+            set
+            {
+                writeStr(0x2DE, value, 32);
+            }
+        }
+        public string longDeviceName
+        {
+            get
+            {
+                return readStr(0x2FE, 64);
+            }
+            set
+            {
+                writeStr(0x2FE, value, 64);
+            }
+        }
+        public string mqtt_group
+        {
+            get
+            {
+                return readStr(0x00000554, 64);
+            }
+            set
+            {
+                writeStr(0x00000554, value, 64);
+            }
+        }
+        public int dgr_sendFlags
+        {
+            get
+            {
+                return readInt(0x460);
+            }
+            set
+            {
+                writeInt(0x460, value);
+            }
+        }
+        public int dgr_recvFlags
+        {
+            get
+            {
+                return readInt(0x464);
+            }
+            set
+            {
+                writeInt(0x464, value);
+            }
+        }
+        public string dgr_name
+        {
+            get
+            {
+                return readStr(0x468, 16);
+            }
+            set
+            {
+                writeStr(0x468, value, 16);
+            }
+        }
+        public byte getPinRole(int index)
+        {
+            if (index > MAX_GPIO)
+                return 0;
+            return readByte(0x033E + index);
+        }
+        public void setPinRole(int index, byte nr)
+        {
+            if (index > MAX_GPIO)
+                return;
+           writeByte(0x033E + index,nr);
+        }
+        public byte getPinChannel(int index)
+        {
+            if (index > MAX_GPIO)
+                return 0;
+            return readByte(0x35E + index);
+        }
+        public void setPinChannel(int index, byte nc)
+        {
+            if (index > MAX_GPIO)
+                return;
+            writeByte(0x35E + index, nc);
+        }
+        public byte getPinChannel2(int index)
+        {
+            if (index > MAX_GPIO)
+                return 0;
+            return readByte(0x37E + index);
+        }
+        public void setPinChannel2(int index, byte nc)
+        {
+            if (index > MAX_GPIO)
+                return;
+            writeByte(0x37E + index, nc);
+        }
+        public byte getChannelType(int index)
+        {
+            if (index > MAX_CHANNELS)
+                return 0;
+            return readByte(0x39E + index);
+        }
+        public void setChannelType(int index, byte nc)
+        {
+            if (index > MAX_CHANNELS)
+                return;
+            writeByte(0x39E + index, nc);
+        }
+        public string ntpServer
+        {
+            get
+            {
+                return readStr(0x478, 32);
+            }
+            set
+            {
+                writeStr(0x478, value, 32);
+            }
+        }
+        public string ping_host
+        {
+            get
+            {
+                return readStr(0x000005A0, 64);
+            }
+            set
+            {
+                writeStr(0x000005A0, value, 64);
+            }
+        }
+        public string initCommandLine
+        {
+            get
+            {
+                return readStr(0x000005E0, 1568);
+            }
+            set
+            {
+                writeStr(0x000005E0, value, 1568);
+            }
+        }
+        public string wifi_ssid2
+        {
+            get
+            {
+                return readStr(0x00000C00, 64);
+            }
+            set
+            {
+                writeStr(0x00000C00, value, 64);
+            }
+        }
+        public string wifi_pass2
+        {
+            get
+            {
+                return readStr(0x00000C84, 64);
+            }
+            set
+            {
+                writeStr(0x00000C84, value, 64);
+            }
+        }
+
+
+        public void saveConfig()
+        {
+            byte crc = 0;
+            raw[0] = (byte)'C';
+            raw[1] = (byte)'F';
+            raw[2] = (byte)'G';
+            raw[3] = (byte)crc;
+        }
+    }
+}
