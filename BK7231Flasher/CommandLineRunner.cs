c9d94c7eae82318375c196c1e2457c56348061e0
divadiow <divadiow@hotmail.com>
2026-03-19 16:48:56 +0000

    forgot commandlinerunner

--- BK7231Flasher/CommandLineRunner.cs
+++ BK7231Flasher/CommandLineRunner.cs
@@ -415,6 +415,7 @@ namespace BK7231Flasher
                 case BKType.BL602:
                 case BKType.BL702:
                 case BKType.XR806:
+                case BKType.XR872:
                     return true;
                 default:
                     return false;
@@ -615,6 +616,8 @@ namespace BK7231Flasher
                     return new RDAFlasher(ct);
                 case BKType.XR806:
                     return new XR806Flasher(ct);
+                case BKType.XR872:
+                    return new XR872Flasher(ct);
                 case BKType.ESP32:
                 case BKType.ESP32C3:
                 case BKType.ESP32S3:
