8fd92aff340d06dd7e37b610944f435ea672b693
NonPIayerCharacter <18557343+NonPIayerCharacter@users.noreply.github.com>
2026-06-09 23:14:09 +0300

    RTL8721DA

--- BK7231Flasher/FormMain.cs
+++ BK7231Flasher/FormMain.cs
@@ -54,6 +54,7 @@ namespace BK7231Flasher
             { BKType.RDA5981,    "RDA5981" },
             { BKType.RTL8710B,   "RTL8710B (AmebaZ)" },
             { BKType.RTL8720D,   "RTL8720DN (AmebaD)" },
+            { BKType.RTL8721DA,   "RTL8721DA (AmebaDplus)" },
             { BKType.RTL87X0C,   "RTL87X0C (AmebaZ2)" },
             { BKType.TR6260,     "TR6260" },
             { BKType.W600,       "W600 (write)" },
@@ -491,8 +492,8 @@ namespace BK7231Flasher
             {
                 case BKType.RTL8710B:
                 case BKType.RTL8720D:
-                case BKType.RTL8721DA:
-                case BKType.RTL8720E:
+                //case BKType.RTL8721DA:
+                //case BKType.RTL8720E:
                     flasher = new RTLFlasher(cts.Token);
                     break;
                 case BKType.RTL87X0C:
@@ -543,6 +544,9 @@ namespace BK7231Flasher
                     break;
                 case BKType.GD32VW553:
                     flasher = new GD32VW553Flasher(cts.Token);
+                    break;
+                case BKType.RTL8721DA:
+                    flasher = new RTLNFlasher(cts.Token);
                     break;
                 default:
                     flasher = new BK7231Flasher(cts.Token);
