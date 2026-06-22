a74dcce79166912ef732a13711e62891f9e3c7bb
divadiow <divadiow@hotmail.com>
2026-06-22 19:10:56 +0100

    Update FormOBKConfig.Designer.cs

--- BK7231Flasher/FormOBKConfig.Designer.cs
+++ BK7231Flasher/FormOBKConfig.Designer.cs
@@ -263,6 +263,7 @@
             // 
             // listViewGPIO
             // 
+            this.listViewGPIO.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
             this.listViewGPIO.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeader1,
             this.columnHeader2,
