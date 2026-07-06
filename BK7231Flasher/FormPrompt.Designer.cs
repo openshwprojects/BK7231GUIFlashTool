7d56877d8d169f7902f8f014d9d1a11d775e3c31
divadiow <62958974+divadiow@users.noreply.github.com>
2026-07-07 00:36:44 +0100

    revise backup file name form (#151)
    * revise backup file prompt form
    
    * Update FormPrompt.Designer.cs
    
    * Update FormPrompt.resx
    
    * revise again

--- BK7231Flasher/FormPrompt.Designer.cs
+++ BK7231Flasher/FormPrompt.Designer.cs
@@ -40,13 +40,13 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(13, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(428, 65);
+            this.label1.Size = new System.Drawing.Size(428, 78);
             this.label1.TabIndex = 0;
             this.label1.Text = resources.GetString("label1.Text");
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(16, 95);
+            this.textBox1.Location = new System.Drawing.Point(16, 110);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(333, 20);
             this.textBox1.TabIndex = 1;
@@ -54,7 +54,7 @@
             // 
             // buttonOk
             // 
-            this.buttonOk.Location = new System.Drawing.Point(362, 121);
+            this.buttonOk.Location = new System.Drawing.Point(362, 136);
             this.buttonOk.Name = "buttonOk";
             this.buttonOk.Size = new System.Drawing.Size(75, 23);
             this.buttonOk.TabIndex = 2;
@@ -64,7 +64,7 @@
             // 
             // buttonCancel
             // 
-            this.buttonCancel.Location = new System.Drawing.Point(16, 121);
+            this.buttonCancel.Location = new System.Drawing.Point(16, 136);
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.Size = new System.Drawing.Size(75, 23);
             this.buttonCancel.TabIndex = 3;
@@ -76,13 +76,13 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(449, 156);
+            this.ClientSize = new System.Drawing.Size(449, 171);
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.buttonOk);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Name = "FormPrompt";
-            this.Text = "FormPrompt";
+            this.Text = "Backup file name";
             this.Load += new System.EventHandler(this.FormPrompt_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
